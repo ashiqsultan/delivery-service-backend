d932f5d827e0eb1c9c68dea8a75a1dee0368b85a
ashiqsultan <ashiqdrive@gmail.com>
2022-11-24 12:48:04 +0530

    Added console error for basic auth function

--- src/services/auth/basicAuth.ts
+++ src/services/auth/basicAuth.ts
@@ -1,25 +1,26 @@
-import AppResponse from "AppResponse";
-import { Request } from "express";
-import { NoAuthorizationHeader } from "../../commonRes";
-import login from "./login";
+import AppResponse from 'AppResponse';
+import { Request } from 'express';
+import { NoAuthorizationHeader } from '../../commonRes';
+import login from './login';
 /**
  * Function to authenticate a user using basic auth.
  */
 const basicAuth = async (req: Request): Promise<AppResponse> => {
   try {
-    const authHeader = req.headers.authorization || "";
+    const authHeader = req.headers.authorization || '';
     if (authHeader) {
       const [username, password] = Buffer.from(
-        authHeader.split(" ")[1],
-        "base64"
+        authHeader.split(' ')[1],
+        'base64'
       )
         .toString()
-        .split(":");
+        .split(':');
       return await login(username, password);
     } else {
       return NoAuthorizationHeader;
     }
   } catch (error) {
+    console.error(error);
     return NoAuthorizationHeader;
   }
 };
